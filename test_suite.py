c3402fb4c79ceac5c550d5fc51011e25425dffc7
maria <duranmaria1129@gmail.com>
2019-04-14 00:57:00 -0400

    changes

--- test_suite.py
+++ test_suite.py
@@ -7,10 +7,10 @@ from test_luma import Test_Luma
 youtube_test = unittest.TestLoader().loadTestsFromTestCase(Test_youtube)
 luma_test = unittest.TestLoader().loadTestsFromTestCase(Test_Luma)
 
-# create a test suite combining test_luma and youtube_test
+# create a test suite combining test_luma + youtube_test
 test_suite = unittest.TestSuite([youtube_test, luma_test])
 
-# run the suite
+
 unittest.TextTestRunner(verbosity=2).run(test_suite)
 
 
