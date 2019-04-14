c9724ae32f21ebdcf0ed6c89d368ea772492f727
maria <duranmaria1129@gmail.com>
2019-04-14 00:50:36 -0400

    initial commit

--- test_suite.py
+++ test_suite.py
@@ -3,11 +3,11 @@ import HtmlTestRunner
 from test_youtube import Test_youtube
 from test_luma import Test_Luma
 
-# get all tests from SearchText and HomePageTest class
+
 youtube_test = unittest.TestLoader().loadTestsFromTestCase(Test_youtube)
 luma_test = unittest.TestLoader().loadTestsFromTestCase(Test_Luma)
 
-# create a test suite combining search_text and home_page_test
+# create a test suite combining test_luma and youtube_test
 test_suite = unittest.TestSuite([youtube_test, luma_test])
 
 # run the suite
